2e145628a6c520f55051cc95da092ac498f89767
s.beriaghdar <fried.code@gmail.com>
2016-01-16 16:52:59 +0330

    initial version

---
+++ index.js
@@ -0,0 +1,22 @@
+var app = require('express')();
+var multer = require('multer');
+
+var upload = multer({ dest: './uploads/'});
+
+/* // uncomment to allow 3rd party domain
+app.all('/upload', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+*/
+
+app.post('/upload', upload.single('file'), function (req, res, next) {
+  console.log(req.body);
+  console.log(req.file);
+  res.json({status:true, id:23213});
+});
+
+app.listen(3000, function(){
+  console.log('listening port 3000');
+});
